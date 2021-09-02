6c4f71b9f82eda1bbf96b39f8652c61736f19f57
Yauheni Shatau <jack13only@gmail.com>
2021-09-02 14:13:31 +0300

    feat: add second solution

--- src/index.js
+++ src/index.js
@@ -2,122 +2,174 @@ module.exports = function solveSudoku(matrix) {
   
 // }
 
-// function solveSudoku(matrix) {
+//function solveSudoku(matrix) {
   let matrix1 = Array.from(matrix)
- // console.log(matrix1);
+  let arrCandidate = []
+  // console.log(matrix1);
   let count = 0
   let stop = 0
   let found = false
   let sum = 0;
   let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   let solutionTemp = [new Set(numbers), new Set(numbers), new Set(numbers), new Set(numbers), new Set(numbers), new Set(numbers), new Set(numbers), new Set(numbers), new Set(numbers)]
+  let solutionTemp2 = []
+  let wrong = [[], [], [], [], [], [], [], [], []]
+
+  //Make SET for each row
   for (let i = 0; i < matrix1.length; i++) {
     for (let j = 0; j < matrix1[i].length; j++) {
       solutionTemp[i].delete(matrix1[i][j])
     }  
   }
 
-
-
- 
+  //Make SET for each element
   for (let i = 0; i < matrix1.length; i++) {
-      if (solutionTemp[i].size === 1) matrix1[i].forEach(function (item) {
-        for (let j = 0; j < matrix1[i].length; j++) {
-         if (matrix1[i][j] === 0) matrix1[i][j] = Array.from(solutionTemp[i].values())[0]
+    for (let j = 0; j < matrix1[i].length; j++) {
+      if (!(Array.isArray(arrCandidate[i]))) arrCandidate[i] = []
+      if (matrix1[i][j] === 0) arrCandidate[i][j] = new Set(solutionTemp[i].values())
+      else {
+        arrCandidate[i][j] = null
+      }
+    }  
+  }
+
+  //Check SET of each element for column
+  for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+          if (arrCandidate[i][j] !== null) {
+            for (let k = 0; k < 9; k++) {
+              arrCandidate[i][j].delete(matrix1[k][j])
+            }
+          } 
+        } 
+  }
+
+  let startK = 0
+  let finishK = 3
+  let startM = 0
+  let finishM = 3
+
+  //Check SET of each element for squares
+  for (let d = 0; d < 9; d++) {
+
+    for (let i = startK; i < finishK; i++) {
+      for (let j = startM; j < finishM; j++) {
+        
+        if (arrCandidate[i][j] !== null) {
+          for (let k = startK; k < finishK; k++) {
+            for (let m = startM; m < finishM; m++) {
+              arrCandidate[i][j].delete(matrix1[k][m])
+            }
+          }
         }
-      })
-  }
-
-  let arr = [[], [], [], [], [], [], [], [], []]
-
-  let arrTest = []
-  let matrix2 = []
-
-  for (let i = 0; i < matrix1.length; i++) {
-    arr[i] = Array.from(solutionTemp[i])
-  } 
- // console.log(arr);
-
-const random = (mas) => {
-  let tt = []
-  for (let i = 0; i < 9; i++) {
-    tt[i] = mas[i].slice().sort(function(){ 
-      return 0.5 - Math.random()
-    })
-  }
-  //console.log(mas);
-  return tt  
-}  
-
-const matrixCopy = (mtrx) => {
-  let t = []
-  for (let i = 0; i < 9; i++) {
-    t[i] = Array.from(mtrx[i])
-  }
-  //console.log(`t = ${t} `);
-  return t
-}
-
-  
-  while (!found) {
-
-    stop++
-
-    arrTest = random(arr)
-    matrix2 = matrixCopy(matrix1)
-
-    sum = 0
-    count = 0
-
-    for (let i = 0; i < 9; i++) {
-      for (let j = 0; j < 9; j++) {
-        if (matrix2[i][j] === 0) matrix2[i][j] = arrTest[i].pop()
       }
     }
 
-       //console.log(matrix2);
+    startK += 3
+    finishK +=3
+    if (startK === 9) {
+      startK = 0
+      finishK = 0
+      startM +=3
+      finishM +=3
+    }
+  }
 
-    for (let j = 0; j < 9; j++) {
+  const matrixCopy = (mtrx) => {
+    let t = []
       for (let i = 0; i < 9; i++) {
-        sum += matrix2[i][j]
-     //   console.log(`matrix2[${i}][${j}] = ${matrix2[i][j]}`);
-    //    console.log(`sum = ${sum}`);
-      } 
-    //  console.log(`Totalsum = ${sum}`);
-      if (sum !== 45) {
-        sum = 0
-        break
-      }
-      else {
-     //   console.log(`count = ${count}`);
-        count++
-        sum = 0
-        }
-      }  
-  //  console.log(matrix2);
-  // console.log(stop);  
-    
-  if (count === 9) {
- //   console.log(`STOOP = ${stop}`);
- //   console.log(`COUNTT = ${count}`);
-    found = true
+      t[i] = Array.from(mtrx[i])
+    }
+    return t
   }
-  if (stop === 450000) found = true
+
+  let matrix2 = []
+
+  let error = 0
+
+  let timeRandNum2
+  let timeArr
+  const timeRand = (i, j) => {
+    timeArr = Array.from(arrCandidate[i][j].values()).sort(function(){ 
+             return 0.5 - Math.random()
+           })
+
+    for (let x = 0; x < timeArr.length; x++) {       
+      if (solutionTemp2[i].has(timeArr[x])) return timeArr[x] 
+    }
+    return null
+  }
+
+  
+// CHECK SUDOKU
+
+  while (!found) {
+    
+    // if (stop % 1000 === 0) console.log(stop);
+    if (stop === 1000000) {
+      found = true    
+      break
+    }
+    error = 0
+    stop++
+
+    matrix2 = matrixCopy(matrix1)
+
+    for (let i = 0; i < 9; i++) {
+      solutionTemp2[i] = new Set(solutionTemp[i].values())
+    }
+     
+
+    for (let i = 0; i < 9; i++) {
+      if (error === 1) break
+      for (let j = 0; j < 9; j++) {
+        if (matrix2[i][j] === 0) {
+
+          timeRandNum2 = timeRand(i, j)
+          if (timeRandNum2 === null) {
+            error = 1
+            break
+          } else matrix2[i][j] = timeRandNum2 
+
+         solutionTemp2[i].delete(matrix2[i][j])
+
+          for (let k = 0; k < 9; k++) {
+            if (matrix2[i][j] === matrix2[i][k] && j !== k) {
+              error = 1
+              break
+            } 
+            if (matrix2[i][j] === matrix2[k][j] && i !== k) {
+              error = 1
+              break
+            } 
+          }
+
+        }
+      }
+    }
+
+    if (error === 1) continue
+    else {
+      console.log('HUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUURAYYYY');
+      found = true
+    }
 
   }
+
   return matrix2
+ 
 }  
 
 // let initial = [
-//   [6, 5, 0, 7, 3, 0, 0, 8, 0],
-//   [0, 0, 0, 4, 8, 0, 5, 3, 0],
-//   [8, 4, 0, 9, 2, 5, 0, 0, 0],
-//   [0, 9, 0, 8, 0, 0, 0, 0, 0],
-//   [5, 3, 0, 2, 0, 9, 6, 0, 0],
-//   [0, 0, 6, 0, 0, 0, 8, 0, 0],
-//   [0, 0, 9, 0, 0, 0, 0, 0, 6],
-//   [0, 0, 7, 0, 0, 0, 0, 5, 0],
-//   [1, 6, 5, 3, 9, 0, 4, 7, 0]
+//   [0, 5, 0, 0, 7, 0, 0, 0, 1],
+//   [8, 7, 6, 0, 2, 1, 9, 0, 3],
+//   [0, 0, 0, 0, 3, 5, 0, 0, 0],
+//   [0, 0, 0, 0, 4, 3, 6, 1, 0],
+//   [0, 4, 0, 0, 0, 9, 0, 0, 2],
+//   [0, 1, 2, 0, 5, 0, 0, 0, 4],
+//   [0, 8, 9, 0, 6, 4, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 7, 0, 0, 0],
+//   [1, 6, 7, 0, 0, 2, 5, 4, 0]
 // ];
 
 // solveSudoku(initial)
